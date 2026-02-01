5467ef0fc781217f288e674b34e45ca63b2e4efb
luq-mne <luqman.yazrie@outlook.com>
2026-02-01 13:20:26 +0800

    added currency id validation and exchange rate functions

--- src/extensions/currency.py
+++ src/extensions/currency.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 fiat_currencies = {}
 crypto_currencies = {}
 
+# updates the local index
 def index_update():
     global fiat_currencies, crypto_currencies
 
@@ -17,6 +18,24 @@ def index_update():
     response = json.loads(response.text)
     data = response["data"]
     for i in data:
-        crypto_currencies[i["code"]] = {"name": i["name"]}
+        crypto_currencies[i["code"]] = {"name": i["name"], "asset_id": i["asset_id"]}
+
+# verify if input currency exists in the index
+def verify_input(currency):
+    if (currency in fiat_currencies or crypto_currencies):
+        return True
+    else:
+        return False
+
+# gets the exchange rate in target currency of a base currency
+def exchange_rate(base_currency, target_currency):
+    global fiat_currencies, crypto_currencies
+
+    if (base_currency in fiat_currencies or crypto_currencies):
+        response = requests.get(f"https://api.coinbase.com/v2/exchange-rates?currency={base_currency}")
+        response = json.loads(response.text)
+        data = response["data"]["rates"]
+        return data[target_currency]
 
 index_update()
+exchange_rate("BTC", "USD")
