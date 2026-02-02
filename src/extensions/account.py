adf528d84ce1202aee7ba68883e7fc4cf3f9101c
luq-mne <luqman.yazrie@outlook.com>
2026-02-03 00:25:04 +0800

    account create

--- src/extensions/account.py
+++ src/extensions/account.py
@@ -2,13 +2,14 @@
 # access_code = hashed and salted access code for the account, used for large transactions or account locks
 # tier = account tier; 1 = member, 2 = novice, 3 = executive, 4 = icon, 5 = elite. refer documentation for privileges.
 
-import sqlite3
+import sqlite3, discord
+from discord.ext import commands
 
 # create an account in the database
 def account_create(account):
     with sqlite3.connect("../database/accounts.db") as conn:
         with conn:
-            conn.execute("CREATE TABLE IF NOT EXISTS user (account INTEGER PRIMARY KEY, tier INTEGER, access_code TEXT, locked INTEGER)")
+            conn.execute("INSERT INTO user VALUES (?)", (account,))            
 
 # check if the account is in the database
 def account_exists(account):
@@ -36,8 +37,39 @@ def access_code_verify(account, access_code):
 def access_code_set(account, access_code, new_access_code):
     with sqlite3.connect("../database/accounts.db") as conn:
         with conn:
-            if access_code_verify(account, access_code) == True:
+            if access_code_verify(account, access_code):
                 conn.execute("UPDATE user SET access_code = ? WHERE account = ?", (new_access_code, account))
                 return True
             else:
                 return False
+
+class account_ext(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        account_group = bot.create_group("account", "Account-related commands.")
+        with sqlite3.connect("../database/accounts.db") as conn:
+            with conn:
+                conn.execute("CREATE TABLE IF NOT EXISTS user (account INTEGER PRIMARY KEY, tier INTEGER DEFAULT 1, access_code TEXT DEFAULT NULL, locked INTEGER DEFAULT 0, created TEXT)")
+
+        # /account create
+        @account_group.command(name="create", description="Create a Yogi account")
+        async def command_create(ctx: discord.ApplicationContext):
+            if account_exists:
+                account_create(ctx.author.id)
+                embed = discord.Embed(
+                    title= "Account Creation Successful",
+                    description= f"{ctx.author.mention}'s has been created successfully!",
+                    color= discord.Color.brand_green()
+                )
+
+                await ctx.respond(embed= embed)
+            else:
+                embed = discord.Embed(
+                    title= "Account Creation Failed",
+                    description= "Your account already exists.",
+                    color= discord.Color.brand_green()
+                )
+                await ctx.respond(embed= embed)
+
+def setup(bot):
+    bot.add_cog(account_ext(bot))
